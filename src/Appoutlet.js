e161e15953cd78467de5bed86b192ef28394d83e
Enofua Etue Divine <divineenofua@gmail.com>
2023-10-19 03:31:29 +0100

    Update Appoutlet.js

--- src/Appoutlet.js
+++ src/Appoutlet.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Outlet } from "react-router-dom"
 import { Link } from "react-router-dom"
  import { FaArrowLeft } from "react-icons/fa";
+ import { FaArrowRight } from "react-icons/fa";
 
 function AppOutlet() {
   return (
     <div>
       <Outlet />
       <p className="home-btn">  <Link className="linked"  to="/"><FaArrowLeft /> <b>Home Page</b></Link></p>
+    <br />
+      <br />
+      <br />
+      <p className="home-btn">  <Link className="linked error"  to="/error"><FaArrowRight/> <b>Error Page</b></Link></p>
       </div>
 
   )
